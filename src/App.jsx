7300495b9c937bd42ee6cf0b41312b742fa23a69
Adrian Tan <adriantan3921@gmail.com>
2025-05-27 13:56:13 +0800

    initial commit of project file

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function Experience() {
+  return (
+    <>
+      <h3>Fullstack Developer</h3>
+      <h4>Sigma Lab Sdn Bhd</h4>
+    </>
+  );
+}
+
+// experience()
+
+function App() {
+  return (
+    <>
+      <h1>Adrian Tan E-Herng</h1>
+      <h2>An upcominig fullstack developer</h2>
+      <Experience />
+    </>
+  )
+}
+
+export default App
